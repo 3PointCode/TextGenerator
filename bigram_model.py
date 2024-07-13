73dc3cc190ade3573baaf000f4d7dc6d7da42e8e
Tomasz Piwoda <tomasz.piwoda@gmail.com>
2024-07-13 14:06:17 +0200

    Added function successors_generator() that generates the successors_map and returns it after reading through every line of the file.

--- bigram_model.py
+++ bigram_model.py
@@ -1,23 +1,27 @@
 import random
 
 reader = open('data/orwell.txt')
-successor_map = {}
-context_window = []
 
-for line in reader:
-    for word in line.split():
-        cleaned_word = word.strip('.,;-"'":?-'!()_{}[]").lower()
-        context_window.append(cleaned_word)
+def successors_generator(reader):
+    successor_map = {}
+    context_window = []
+    for line in reader:
+        for word in line.split():
+            cleaned_word = word.strip('.,;-"'":?-'!()_{}[]").lower()
+            context_window.append(cleaned_word)
 
-        if len(context_window) == 2:
-            key = context_window[0]
-            value = context_window[1]
-            if key in successor_map:
-                successor_map[key].append(value)
-            else:
-                successor_map[key] = [value]
-            context_window.pop(0)
+            if len(context_window) == 2:
+                key = context_window[0]
+                value = context_window[1]
+                if key in successor_map:
+                    successor_map[key].append(value)
+                else:
+                    successor_map[key] = [value]
+                context_window.pop(0)
 
+    return successor_map
+
+successor_map = successors_generator(reader)
 random.seed(2)
 word = input("Type anything (only one word will be used for context window): ")
 length = int(input("How long do you want your text to be: "))
